4762cb11024d5d7cdc8100c0ddad01df9b443606
Thanate Janyaswasdi <mugenboat@gmail.com>
2024-03-25 13:35:38 +0700

    remove notion

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,7 +7,7 @@ import notion from "/public/notion.png";
 const Contact = () => {
   return (
     <div>
-      <div className="flex w-[120px] justify-between">
+      <div className="flex w-[80px] justify-between">
         <div className="w-[35px] ">
           <a
             href="https://www.linkedin.com/in/thanate-janyaswasdi"
@@ -31,16 +31,6 @@ const Contact = () => {
             rel="noopener noreferrer"
           >
             <Image src={github} loading="lazy" width={35} height={35} alt="" />
-          </a>
-        </div>
-
-        <div className="w-[35px] ">
-          <a
-            href="https://butternut-arrow-da3.notion.site/Development-79430cde50cf42b7a3fa98fd438e96ae"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image src={notion} loading="lazy" width={35} height={35} alt="" />
           </a>
         </div>
       </div>
