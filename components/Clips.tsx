a7ab9c5eb897ccb9a2893f11351c92f43b2a3e62
Thanate Janyaswasdi <mugenboat@gmail.com>
2025-04-07 22:03:51 +0700

    clean code

--- components/Clips.tsx
+++ components/Clips.tsx
@@ -3,6 +3,16 @@ import ProjectCard from "./ProjectCard";
 import Image from "next/image";
 
 const Experience = () => {
+  const tiktoks = [
+    "https://www.youtube.com/embed/CQO9gMIIuys?si=68ick9LbePBt5nFg",
+    "https://youtube.com/embed/VWRyhuojz7s",
+    "https://youtube.com/embed/CbW4DvW_DOc",
+    "https://youtube.com/embed/hS4rdwi0Lxc",
+    "https://youtube.com/embed/JXUhg9WXCnY",
+    "https://youtube.com/embed/KvML_nF5ZMo",
+    "https://youtube.com/embed/nmGToClGjnQ",
+  ];
+
   return (
     <div>
       <h2 className="flex justify-center items-center text-[15px] text-green-300 my-3">
@@ -20,78 +30,22 @@ const Experience = () => {
       </h2>
 
       <div className="w-[80%] mx-auto mt-6 grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-1 gap-[3rem] h-[100%] items-center justify-items-center">
-        <div className="text-green-300">
-          <div className=" rounded-lg border border-gray-200 w-[260px] h-[250px]">
-            <div className="flex justify-center items-center">
-              <iframe
-                className="p-3"
-                src="https://www.youtube.com/embed/CQO9gMIIuys?si=68ick9LbePBt5nFg"
-                allowFullScreen
-                loading="lazy"
-                width="250"
-                height="250"
-              />
+        {tiktoks.map((tiktok, index) => (
+          <div className="text-green-300" key="index">
+            <div className=" rounded-lg border border-gray-200 w-[260px] h-[250px]">
+              <div className="flex justify-center items-center">
+                <iframe
+                  className="p-3"
+                  src={tiktok}
+                  allowFullScreen
+                  loading="lazy"
+                  width="250"
+                  height="250"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="text-green-300">
-          <div className=" rounded-lg border border-gray-200 w-[260px] h-[250px]">
-            <div className="flex justify-center items-center">
-              <iframe
-                className="p-3"
-                src="https://youtube.com/embed/hS4rdwi0Lxc"
-                allowFullScreen
-                loading="lazy"
-                width="250"
-                height="250"
-              />
-            </div>
-          </div>
-        </div>
-        {/* https://youtube.com/shorts/KvML_nF5ZMo */}
-        <div className="text-green-300">
-          <div className=" rounded-lg border border-gray-200 w-[260px] h-[250px]">
-            <div className="flex justify-center items-center">
-              <iframe
-                className="p-3"
-                src="https://youtube.com/embed/JXUhg9WXCnY"
-                allowFullScreen
-                loading="lazy"
-                width="250"
-                height="250"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="text-green-300">
-          <div className=" rounded-lg border border-gray-200 w-[260px] h-[250px]">
-            <div className="flex justify-center items-center">
-              <iframe
-                className="p-3"
-                src="https://youtube.com/embed/KvML_nF5ZMo"
-                allowFullScreen
-                loading="lazy"
-                width="250"
-                height="250"
-              />
-            </div>
-          </div>
-        </div>
-        {/* </div> */}
-        <div className="text-green-300">
-          <div className=" rounded-lg border border-gray-200 w-[260px] h-[250px]">
-            <div className="flex justify-center items-center">
-              <iframe
-                className="p-3"
-                src="https://youtube.com/embed/nmGToClGjnQ"
-                allowFullScreen
-                loading="lazy"
-                width="250"
-                height="250"
-              />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
       <h2 className="bt-5 flex justify-center items-center text-[15px] text-green-300 my-3">
         more clips is coming soon
