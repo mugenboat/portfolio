cac1fbced48734901f0606933a44e2efd9c0c217
Thanate Janyaswasdi <mugenboat@gmail.com>
2025-05-21 11:01:44 +0700

    change

--- components/Clips.tsx
+++ components/Clips.tsx
@@ -49,6 +49,12 @@ const Experience = () => {
         this is some of my idea brainstorming with my friends since when i had
         been university .
       </h2>
+      <h2 className="flex justify-center items-center text-[15px] text-green-300 my-3">
+        watch me more in Tiktok :
+        <a href="https://www.tiktok.com/@boattheblur" className="text-blue-300">
+          boattheblur
+        </a>
+      </h2>
       <div className="w-[80%] mx-auto mt-6 grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-1 gap-[3rem] h-[100%] items-center justify-items-center">
         {facebooks.map(facebook => (
           <div key={facebook.id} className="text-green-300">
@@ -72,17 +78,8 @@ const Experience = () => {
         This is sample of my trying editing video, there more because im still
         learning .
       </h2>
-      <h2 className="flex justify-center items-center text-[15px] text-green-300 my-3">
-        watch me more in Tiktok :
-        <a href="https://www.tiktok.com/@boattheblur" className="text-blue-300">
-          boattheblur
-        </a>
-      </h2>
-      <h2 className="flex justify-center items-center text-[15px] text-green-300 my-3">
-        Ps. I just created tiktok account on 26 Feb 2025.
-      </h2>
 
-      <div className="w-[80%] mx-auto mt-6 grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-1 gap-[3rem] h-[100%] items-center justify-items-center">
+      {/* <div className="w-[80%] mx-auto mt-6 grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-1 gap-[3rem] h-[100%] items-center justify-items-center">
         {tiktoks.map((tiktok, index) => (
           <div className="text-green-300" key={tiktok.id}>
             <div className=" rounded-lg border border-gray-200 w-[260px] h-[250px]">
@@ -99,7 +96,7 @@ const Experience = () => {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
       <h2 className="bt-5 flex justify-center items-center text-[15px] text-green-300 my-3">
         more clips is coming soon
       </h2>
